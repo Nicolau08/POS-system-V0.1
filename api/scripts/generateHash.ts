34053084768fadb3ffee97b53d1979d0a7dc9346
Nicolau08 <nicolau@giga-it.co.mz>
2026-05-08 11:18:24 +0200

    feat: promote local recovered version without electron build artifacts

--- api/scripts/generateHash.ts
+++
@@ -1,8 +0,0 @@
-import bcrypt from "bcrypt";
-
-async function run() {
-  const hash = await bcrypt.hash("1234", 10);
-  console.log(hash);
-}
-
-run();
--- api/utils/httpResponse.js
+++
@@ -1,7 +0,0 @@
-export {
-  ApiError,
-  HttpError,
-  handleControllerError,
-  sendError,
-  sendSuccess,
-} from './response.js';
